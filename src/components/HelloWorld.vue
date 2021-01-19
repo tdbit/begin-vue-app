c714955616f3ae94c6eb8a293ab70f6072523644
Tom <tdbit@users.noreply.github.com>
2021-01-19 14:58:49 -0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <h1>{{ message }}</h1>
     <h2>Change me</h2>
     <p>
+      This is a change
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
